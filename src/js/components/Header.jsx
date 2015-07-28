23438f5109e6d0d7e15c5b3e8179b3821c13f86c
Kenneth Chung <ken70r@gmail.com>
2015-07-27 22:44:51 -0700

    Inline ness logo
    Saves an HTTP request. Inline it since it's only < 500B.

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -4,9 +4,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <header className="Site-header">
-        <a href="/">
-          <img src="/images/ness.gif" />
-        </a>
+        <a className="Site-logo" href="/" />
       </header>
     );
   }
